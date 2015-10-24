91cc43179b03f5ce1cfc294fde76b425946293e3
Roberto Nerici <rnerici@gmail.com>
2015-10-24 18:08:09 +0100

    Now tracks connected clients

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var clients = [];
+var connectedClients = {};
 
 app.use(express.static(__dirname + '/../client'));
 app.use('/public', express.static(__dirname + '/../public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/../client/index.html');
+});
+
+app.get('/clients', function(req, res) {
+    res.send(connectedClients);
 });
 
 server = http.listen(3000, function() {
@@ -19,20 +23,39 @@ server = http.listen(3000, function() {
 
 
 io.on('connection', function (socket) {
-  console.log('User connected');
+    console.log('User connected');
 
-  socket.on('disconnect', function() {
-    console.log('User disconnected');
-  });
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+        removeClient(socket);
+    });
 
-  socket.on('new-player', function(playerName) {
-      console.log('The name of player: ' + socket.id + ' is ' + playerName);
-      socket.emit('loginEvent', createLoggedInEvent());
-  })
+    socket.on('new-player', function(playerName) {
+        addClient(playerName, socket);
+        socket.emit('loginEvent', createLoggedInEvent(playerName));
+    })
 });
 
-function createLoggedInEvent() {
+function createLoggedInEvent(playerName) {
     return {
-        type: 'loggedIn'
+        type: 'loggedIn',
+        player: {
+            name: playerName,
+            rank: 1
+        }
     }
+}
+
+function addClient(playerName, socket) {
+    var client = {
+        name: playerName,
+        id: socket.id
+    }
+    connectedClients[socket.id] = client;
+    console.log(connectedClients);
+}
+
+function removeClient(socket) {
+    delete connectedClients[socket.id];
+    console.log(connectedClients);
 }
