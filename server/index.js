6e7426e70fb17dee52a9e98c232242b51ef49ade
Povilas Dauksas <povilas.d@gmail.com>
2015-10-24 13:53:19 +0100

    Setup for serving up static content

---
+++ server/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var server;
+
+app.use(express.static(__dirname + "/../client"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/../client/index.html");
+});
+
+server = app.listen(3000, function() {
+    var port = server.address().port;
+
+    console.log("TWOTEK app is listening at %s", port);
+});
