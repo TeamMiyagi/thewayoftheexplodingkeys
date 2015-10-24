78d544667c8d0c72c9cd6f7c4855f4d3f58467c4
Roberto Nerici <rnerici@gmail.com>
2015-10-24 22:05:23 +0100

    Added player spite

--- client/js/game/game.js
+++ client/js/game/game.js
@@ -21,14 +21,18 @@ var Game = function(boutInfo) {
 
 function preload() {
     console.log("boutInfo: ", bout);
+    game.load.spritesheet('player', 'assets/images/sprites/player.png', 48, 64, 4);
 }
 
 function create() {
     game.stage.backgroundColor = 0xaaaaaa;
     $('canvas').addClass('center-block');
 
-    player1Text = game.add.text(0, 0, bout.player1Id, {});
-    player2Text = game.add.text(500, 0, bout.player2Id, { align: 'right' });
+    player1Text = game.add.text(0, 0, bout.player1.name, {});
+    player2Text = game.add.text(500, 0, bout.player2.name, { align: 'right' });
+
+    player1 = game.add.sprite(0, 200, 'player');
+    player1.animations.add('ready', [0, 1, 2, 3], 4, true);
 }
 
 function update() {
