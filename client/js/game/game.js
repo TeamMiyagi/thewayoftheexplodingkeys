79e1590dda872796060458da5227e4bf696f00cd
Roberto Nerici <rnerici@gmail.com>
2015-10-24 22:39:38 +0100

    Player 2's name now correctly aligned

--- client/js/game/game.js
+++ client/js/game/game.js
@@ -3,6 +3,7 @@ var game;
 
 var player1Text;
 var player2Text;
+var player1;
 
 var Game = function(boutInfo) {
     bout = boutInfo;
@@ -29,10 +30,13 @@ function create() {
     $('canvas').addClass('center-block');
 
     player1Text = game.add.text(0, 0, bout.player1.name, {});
-    player2Text = game.add.text(500, 0, bout.player2.name, { align: 'right' });
+    player2Text = game.add.text(0, 0, bout.player2.name, { align: 'right', boundsAlignH: 'right' });
+    player2Text.setTextBounds(0, 0, 800, 600);
 
-    player1 = game.add.sprite(0, 200, 'player');
-    player1.animations.add('ready', [0, 1, 2, 3], 4, true);
+    player1 = game.add.sprite(0, 300, 'player');
+    player1.scale.setTo(4, 4);
+    player1.animations.add('ready', [0, 1, 2, 3, 2, 1], 3, true);
+    player1.animations.play('ready');
 }
 
 function update() {
