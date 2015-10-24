ffeece0d23bb06f59a79ce317912392356479c40
Adam Cook <adam.cook@bbc.co.uk>
2015-10-24 14:46:24 +0100

    add more js client build steps, move public files into public folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,29 @@ var babel = require('babel/register');
 var uglify = require('gulp-uglify');
 
 var paths = {
-    client: ['client/js/**/*.js']
+    client: ['client/js/game/*.js']
 };
 
-gulp.task('build-client', function () {
+gulp.task('copy-client', function() {
+    gulp.src('client/js/lib/phaser.min.js')
+        .pipe(gulp.dest('./public/js/'));
+
+    gulp.src('client/assets/**/*')
+        .pipe(gulp.dest('./public/assets/'));
+})
+
+gulp.task('build-client', ['copy-client'], function() {    
     var b = browserify();
+
     b.transform(babelify);
     b.add('client/js/game/game.js');
     return b.bundle()
             .pipe(source('main.min.js'))
-            .pipe(gulp.dest('./client/js/'));
+            .pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['browserify']);
+gulp.task('watch', function() {
+    gulp.watch(paths.client, ['build-client']);
 });
+
+gulp.task('default', ['build-client'], function() {});
