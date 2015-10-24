7506cd3972eaa66eda20b4a272e5c7c121392f81
Adam Cook <adam.cook@bbc.co.uk>
2015-10-24 13:37:53 +0100

    Add basic client stuff

---
+++ client/js/game/game.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+var run = require('gulp-run');
+var babelify = require('babelify');
+var mocha = require('gulp-mocha');
+var babel = require('babel/register');
+
+var paths = {
+    scripts: ['js/**/*.jsx', 'js/**/*.js']
+};
+
+gulp.task('browserify', function () {
+    var b = browserify();
+    b.transform(reactify);
+    b.transform(babelify);
+    b.add('js/app.jsx');
+    return b.bundle()
+            .pipe(source('main.js'))
+            .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['browserify']);
+});
+
+gulp.task('test', function () {
+    return gulp.src('tests/**/*.js')
+        .pipe(mocha({
+            compilers: {
+                js: babel
+            }
+        }));
+});
