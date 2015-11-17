3b74fd49db531d5c2ae742e7d75f5d7bc8a57e2f
Roberto Nerici <rnerici@gmail.com>
2015-11-17 22:35:40 +0000

    Added skeleton of jasmine testing for GameService

---
+++ server/gameservice/spec/GameServiceSpec.js
@@ -0,0 +1,7 @@
+describe('The Game Service', function() {
+    var service = require('../../gameservice');
+
+    it('can be created', function() {
+        expect(service).not.toBe(null);
+    });
+});
